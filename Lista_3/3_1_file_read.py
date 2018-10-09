2436d26f727d49a5de214681a2424a1e4a872643
martapienkowska <martucha_p@wp.pl>
2018-10-09 19:26:58 +0200

    Celsius, Fahrenheit conversion. Module, reading and writing to file.

--- Lista_3/3_1_file_read.py
+++ Lista_3/3_1_file_read.py
@@ -5,7 +5,7 @@ with open('celcjusz.txt', 'r') as t:
 fahrenheit = open("fahrenheit.txt", 'w')
 for i in read:
     l = []
-    d = module_3_1.cel_fahr(int(i))
+    d = module_3_1.cel_fahr(float(i))
     fahrenheit.write(str(d)+"\n")
 fahrenheit.close()
 
--- sql/trail_parts.sql
+++
@@ -1,3 +0,0 @@
-select tt.* from
-(select unnest(parts) as w_id from planet_osm_rels where id=1593519) t
-join planet_osm_ways tt on t.w_id = tt.id;
