bb310374b5c879c4d3b2e3ca626a0cd7c6cb0f9c
pkopszak <piotr.kopszak@datarino.com>
2018-11-15 20:35:43 +0100

    Corrected class exercises

--- classes/rp_exc2.py
+++ classes/rp_exc2.py
@@ -5,7 +5,9 @@ class Pets:
 
     def __init__(self, dogs):
         self.dogs = dogs
-        
+    def walk(self):
+        for dog in self.dogs:
+            print( "{} walks".format(dog.name))
         
 # Parent class
 class Dog:
@@ -18,7 +20,10 @@ class Dog:
         self.name = name
         self.age = age
         self.is_hungry = True
-    
+
+    def walk(self):
+        return "{} walks".format(self.name)
+
     def eat(self):
         self.is_hungry = False
 
@@ -60,12 +65,24 @@ for dog in my_dogs:
 	
 print("And they're all {}s, of course.".format(Dog.species))
 
-not_feeded = Dog("every", "none").is_hungry
-print(not_feeded)
+for dogs in my_dogs:
+    dogs.eat()
 
-feeded = not_feeded.eat()
-feeded = feeded.is_hungry
-print(feeded)
+are_my_dogs_hungry = False
+for dog in my_pets.dogs:
+    if dog.is_hungry:
+        are_my_dogs_hungry = True
 
+
+if are_my_dogs_hungry:
+    print("My dogs are hungry.")
+else:
+    print("My dogs are not hungry.")
+
+for dog in my_dogs:
+    a = dog.walk()
+    print(a)
+
+my_pets.walk()
 
 
